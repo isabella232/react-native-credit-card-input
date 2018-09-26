8603442f083ef33202705478c34e8b8eac826208
Ruchika Gupta <ruchika@geekyants.com>
2018-09-26 20:15:41 +0530

    UI fix and redesign

--- src/CCFieldValidator.js
+++ src/CCFieldValidator.js
@@ -4,9 +4,9 @@ import values from "lodash.values";
 import every from "lodash.every";
 
 const toStatus = validation => {
-  return validation.isValid ? "valid" :
-         validation.isPotentiallyValid ? "incomplete" :
-         "invalid";
+  return validation.isValid
+    ? "valid"
+    : validation.isPotentiallyValid ? "incomplete" : "invalid";
 };
 
 const FALLBACK_CARD = { gaps: [4, 8, 12], lengths: [16], code: { size: 3 } };
@@ -16,23 +16,26 @@ export default class CCFieldValidator {
     this._validatePostalCode = validatePostalCode;
   }
 
-  validateValues = (formValues) => {
+  validateValues = formValues => {
     const numberValidation = valid.number(formValues.number);
     const expiryValidation = valid.expirationDate(formValues.expiry);
     const maxCVCLength = (numberValidation.card || FALLBACK_CARD).code.size;
     const cvcValidation = valid.cvv(formValues.cvc, maxCVCLength);
 
-    const validationStatuses = pick({
-      number: toStatus(numberValidation),
-      expiry: toStatus(expiryValidation),
-      cvc: toStatus(cvcValidation),
-      name: !!formValues.name ? "valid" : "incomplete",
-      postalCode: this._validatePostalCode(formValues.postalCode),
-    }, this._displayedFields);
+    const validationStatuses = pick(
+      {
+        number: toStatus(numberValidation),
+        expiry: toStatus(expiryValidation),
+        cvc: toStatus(cvcValidation),
+        name: !!formValues.name ? "valid" : "incomplete",
+        postalCode: this._validatePostalCode(formValues.postalCode)
+      },
+      this._displayedFields
+    );
 
     return {
       valid: every(values(validationStatuses), status => status === "valid"),
-      status: validationStatuses,
+      status: validationStatuses
     };
   };
 }
